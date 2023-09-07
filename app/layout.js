6ee2d3c25fe232cd4bbf6a9a5f2f7234a9de25f4
iamgx <imaxing@126.com>
2023-09-07 18:30:18 +0800

    upt: layout 加载krpano

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import classNames from 'classnames'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export const metadata = { title: 'Pages' }
 export default function RootLayout({ children }) {
   return (
     <html lang='zh'>
+      <Script src='https://www.yeecai.com/cheshi/3dfly/dist/krpano/krpano.js' />
       <body className={classNames(inter.className, `bg-[#fff] text-[#212121] dark:text-[#fff] dark:bg-[#212121]`)}>
         {children}
       </body>
