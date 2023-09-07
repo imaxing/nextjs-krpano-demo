c836a76bc05cfa0f501e2cfb08133ba1fdd47b77
iamgx <imaxing@126.com>
2023-09-07 11:23:06 +0800

    upt: 模型预览组件

--- components/model-viewer.js
+++ components/model-viewer.js
@@ -1,17 +1,67 @@
 'use client'
 
-import React, { Suspense } from 'react'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import React, { useEffect } from 'react'
+import { useWindowSize } from '@uidotdev/usehooks'
+export default function ModelViewer({ model }) {
+  const size = useWindowSize()
+  useEffect(() => {
+    let camera, scene, renderer
 
-export default function ModelViewer({ path }) {
-  const { scene } = useLoader(GLTFLoader, path)
+    const init = () => {
+      const container = document.getElementById('container')
 
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[0, 10, 10]} />
-      <Suspense fallback={null}>{scene ? <primitive object={scene} /> : null}</Suspense>
-    </Canvas>
-  )
+      camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.25, 20)
+      camera.position.set(2.5, 1.5, 3.0)
+
+      scene = new THREE.Scene()
+
+      new RGBELoader().setPath('').load(model.background, texture => {
+        texture.mapping = THREE.EquirectangularReflectionMapping
+
+        scene.background = texture
+        scene.environment = texture
+
+        render()
+
+        const loader = new GLTFLoader().setPath('')
+        loader.load(model.url, gltf => {
+          gltf.scene.scale.set(model.x, model.y, model.z)
+          scene.add(gltf.scene)
+          render()
+        })
+      })
+
+      renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(size.width, size.height)
+      renderer.toneMapping = THREE.ACESFilmicToneMapping
+      renderer.toneMappingExposure = 1
+      container.appendChild(renderer.domElement)
+
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.addEventListener('change', render)
+      controls.minDistance = 2
+      controls.maxDistance = 10
+      controls.target.set(0, 0.5, -0.2)
+      controls.update()
+
+      camera.aspect = size.width / size.height
+      camera.updateProjectionMatrix()
+
+      renderer.setSize(size.width, size.height)
+
+      render()
+    }
+
+    const render = () => renderer.render(scene, camera)
+
+    init()
+    render()
+  }, [model, size])
+
+  return <div id='container'></div>
 }
