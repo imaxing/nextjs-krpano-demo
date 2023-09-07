9439a68f9a5809d4de760727e47f2e62ff31c9d4
iamgx <imaxing@126.com>
2023-09-07 09:23:25 +0800

    upt: loading 组件

--- components/loading.js
+++ components/loading.js
@@ -1,10 +1,11 @@
+import FullFixedWrapper from '@/components/full-fixed-wrapper'
 export default function Loading({ text, background }) {
   return (
-    <div
+    <FullFixedWrapper
       style={{ background: background || 'rgba(0, 0, 0, 0.8)' }}
-      className='fixed w-screen h-screen left-0 top-0 z-[20] flex items-center justify-center text-[#fff]'
+      className='flex items-center justify-center text-[#fff]'
     >
       {text || 'loading...'}
-    </div>
+    </FullFixedWrapper>
   )
 }
