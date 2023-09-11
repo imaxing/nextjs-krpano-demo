6d6225c481c8aa69322b6c62bd110b5acd711ba8
iamgx <imaxing@126.com>
2023-09-11 18:22:10 +0800

    upt: react toaast

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import 'tailwindcss/tailwind.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
 import classNames from 'classnames'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = { title: 'Pages' }
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
       <Script src='https://www.yeecai.com/cheshi/3dfly/dist/krpano/krpano.js' />
       <body className={classNames(inter.className, `bg-[#fff] text-[#212121] dark:text-[#fff] dark:bg-[#212121]`)}>
         {children}
+        <ToastContainer />
       </body>
     </html>
   )
