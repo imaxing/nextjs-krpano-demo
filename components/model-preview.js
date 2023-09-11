01c822d3db9aa3b553d3b3145a068eca3e3e6acf
iamgx <imaxing@126.com>
2023-09-11 18:08:45 +0800

    upt: 模型预览组件

---
+++ components/model-preview.js
@@ -0,0 +1,19 @@
+'use client'
+
+import ModelViewer from '@/components/model-viewer'
+import FullFixedWrapper from '@/components/full-fixed-wrapper'
+
+export default function ModelPreview(props) {
+  return (
+    <FullFixedWrapper className='flex items-center justify-center'>
+      <ModelViewer model={props.model} />
+
+      <button
+        className='fixed left-[20px] z-[99999999999999] top-[20px] bg-[#0F0F0F] text-[#fff] px-[15px] py-[8px] rounded-md'
+        onClick={() => props.onBack?.()}
+      >
+        返回
+      </button>
+    </FullFixedWrapper>
+  )
+}
