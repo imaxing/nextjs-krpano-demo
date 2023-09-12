d53147c228955d2c4116fe969c1af413a05e4c66
iamgx <imaxing@126.com>
2023-09-12 10:48:22 +0800

    chore: class to classname

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 export default function App() {
   return (
-    <div class='w-[50%] mx-auto my-[10vh] text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white'>
-      <Link class='block w-full p-6  border-b border-gray-200 rounded-t-lg dark:border-gray-600' href='/example'>
+    <div className='w-[50%] mx-auto my-[10vh] text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white'>
+      <Link className='block w-full p-6  border-b border-gray-200 rounded-t-lg dark:border-gray-600' href='/example'>
         渲染示例
       </Link>
-      <Link class='block w-full p-6  border-b border-gray-200 rounded-t-lg dark:border-gray-600' href='/spots'>
+      <Link className='block w-full p-6  border-b border-gray-200 rounded-t-lg dark:border-gray-600' href='/spots'>
         添加热点
       </Link>
     </div>
